8e3b9fccc03594235b48abd30c8769bb86537dac
lucas <joselucas20048jl@gmail.com>
2025-08-29 22:47:23 -0300

    bug fix

--- frontend/src/services/githubApi.ts
+++ frontend/src/services/githubApi.ts
@@ -1,15 +1,15 @@
-import type { GitHubRepo, GitHubUser } from "../types";
+import type { GitHubUser, GitHubRepo } from "../types";
 
 const API_BASE = "http://localhost:3001/api/github";
 
-export const fetchUser = async (username: string): Promise<GitHubUser> => {
+export async function fetchUser(username: string): Promise<GitHubUser> {
   const res = await fetch(`${API_BASE}/users/${username}`);
-  if (!res.ok) throw new Error("Error while looking for user.");
+  if (!res.ok) throw new Error("Erro ao buscar usuário");
   return res.json();
-};
+}
 
-export const fetchRepos = async (username: string): Promise<GitHubRepo[]> => {
+export async function fetchRepos(username: string): Promise<GitHubRepo[]> {
   const res = await fetch(`${API_BASE}/users/${username}/repos`);
   if (!res.ok) throw new Error("Erro ao buscar repositórios");
   return res.json();
-};
+}
